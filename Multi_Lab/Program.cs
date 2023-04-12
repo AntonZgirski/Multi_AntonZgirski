a79b4ec5dc4ef69ce181cba706d7da1eba681595
AntonZgriski <anton.zgirski@gmail.com>
2023-04-12 15:35:51 +0300

    done

---
+++ Multi_Lab/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace Multi_Lab
+{
+  internal class Program
+  {
+    static void Main(string[] args)
+    {
+      var options = new ParallelOptions() { MaxDegreeOfParallelism = 5 };
+
+      Parallel.For(1, 100000, options, (x) => {
+        int c = 0;
+        for (int i = 1; i <= x; i++)
+        {
+          if (x % i == 0) c++;
+        }
+        if (c == 2) Console.WriteLine(x);
+      });
+    }
+  }
+}
